23b90a2e215952ca68a8258725d11d5b94caa1b4
Nelick <maximilian.champroux@gmail.com>
2026-02-17 20:41:58 +0100

    feat: Create Appheader component

---
+++ app/components/Appheader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import "../assets/style.css";
+import { RouterLink } from "vue-router";
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">KroneHub</h1>
+    <nav>
+      <ul>
+       <li><NuxtLink to="/">Home</NuxtLink></li>
+       <li><NuxtLink to="/about">About</NuxtLink></li>
+       <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style scoped></style>
